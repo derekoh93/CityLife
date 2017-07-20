bb02a91b147f3d3e281508cb9178bf821611d24e
Derek Oh <derek.oh93@gmail.com>
2017-07-20 10:12:25 -0700

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,8 +15,8 @@ module.exports = function (grunt) {
 
   // Automatically load required grunt tasks
   require('jit-grunt')(grunt, {
-    useminPrepare: 'grunt-usemin',
-    buildcontrol: 'grunt-build-control'
+      useminPrepare: 'grunt-usemin',
+      buildcontrol: 'grunt-build-control'
   });
 
   // Configurable paths
@@ -24,30 +24,28 @@ module.exports = function (grunt) {
     app: 'app',
     dist: 'dist'
   };
-  
-     // Define the configuration for all the tasks
-     grunt.initConfig({
 
-       // Project settings
-       config: config,
+  // Define the configuration for all the tasks
+  grunt.initConfig({
 
-       buildcontrol: {
-         options: {
-           dir: 'dist',
-           commit: true,
-           push: true,
-           message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-         },
-         pages: {
-           options: {
-             remote: 'git@github.com:your_github_user/your_webapp.git',
-             branch: 'gh-pages'
-           }
-         }
-       },
+    // Project settings
+    config: config,
 
-       // Watches files for changes and runs tasks based on the changed files
-       watch: {
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:derekoh93/CityLife.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
+
 
      // Watches files for changes and runs tasks based on the changed files
      watch: {
